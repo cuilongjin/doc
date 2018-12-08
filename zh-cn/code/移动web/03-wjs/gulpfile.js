9fbdb32c5c09675e62cddd926d97c71cfca4af34
cuilongjin <cui3155@gmail.com>
2018-12-08 08:34:34 +0800

    wjs

---
+++ zh-cn/code/移动web/03-wjs/gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp')
+var connect = require('gulp-connect')
+gulp.task('connect', function () {
+  connect.server({
+    root: './',
+    livereload: true
+  })
+})
+gulp.task('html', function () {
+  gulp.src('./*.html')
+      .pipe(gulp.dest('./'))
+      .pipe(connect.reload())
+})
+
+gulp.task('watch', function () {
+  gulp.watch(['./*.html'], ['html'])
+})
+
+gulp.task('default', ['connect', 'watch'])
