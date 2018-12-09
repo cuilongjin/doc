9e11cda391a8182dcec246b477c98a2b24aacfd1
cuilongjin <cui3155@gmail.com>
2018-12-09 19:29:29 +0800

    更新并删除node_modules组件

--- zh-cn/code/移动web/03-wjs/gulpfile.js
+++ zh-cn/code/移动web/03-wjs/gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp')
+var less = require('gulp-less')
+var path = require('path')
 var connect = require('gulp-connect')
 gulp.task('connect', function () {
   connect.server({
@@ -6,10 +8,23 @@ gulp.task('connect', function () {
     livereload: true
   })
 })
+
+
+gulp.task('less', function () {
+  return gulp.src('./less/*.less')
+      .pipe(less({
+        paths: [path.join(__dirname, 'less', 'includes')]
+      }))
+      .pipe(gulp.dest('./css'))
+})
 gulp.task('html', function () {
   gulp.src('./*.html')
-      .pipe(gulp.dest('./'))
+      // .pipe(gulp.dest('./'))
       .pipe(connect.reload())
+})
+
+gulp.task('watch-less', function () {
+  gulp.watch(['./less/*.less'], ['less'])
 })
 
 gulp.task('watch', function () {
